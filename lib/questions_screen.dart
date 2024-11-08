3b2b33e9a1b569e51b09bcdb6e22982dcca60982
Karan <karankhandekar028@gmail.com>
2024-11-08 18:19:49 +0530

    managed the question index

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,13 +12,24 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++; //increment by 1
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity, //as wide as possible
-      child: Container( //refactored with container for adding margin
+      child: Container(
+        //refactored with container for adding margin
         margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -33,7 +44,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(answer, () {});
+              return AnswerButton(answer, answerQuestion);
             }),
             // AnswerButton(currentQuestion.answers[0], () {}),
             // AnswerButton(currentQuestion.answers[1], () {}),
